82e5952e76190043748531d0fc62d2e6a8d775e1
Brendan J. Kelly <brendank@upenn.edu>
2024-02-07 14:33:26 -0500

    Update motionlab site.

--- scripts/distill_site_render.R
+++ scripts/distill_site_render.R
@@ -2,4 +2,14 @@ library(rmarkdown)
 library(distill)
 library(postcards)
 
+system("cd ~/motionlab-site; git pull")
+Sys.sleep(45)
+
 render_site()
+
+system("cd ~/motionlab-site; git add *")
+system("cd ~/motionlab-site; git commit -m 'Update motionlab site.'")
+system("cd ~/motionlab-site; git push origin main")
+
+
+
