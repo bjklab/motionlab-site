246c8b50447e9c9dd18d91e7ff387a6c63d7283a
Brendan J. Kelly <brendank@upenn.edu>
2024-04-06 23:11:45 -0400

    Add Project development flow figure.

--- docs/scripts/distill_site_render.R
+++ docs/scripts/distill_site_render.R
@@ -7,7 +7,7 @@ Sys.sleep(15)
 
 rmarkdown::render_site()
 
-Sys.sleep(15)
+Sys.sleep(30)
 system("cd ~/motionlab-site; git add *")
 system("cd ~/motionlab-site; git commit -m 'Update motionlab site.'")
 system("cd ~/motionlab-site; git push origin main")
